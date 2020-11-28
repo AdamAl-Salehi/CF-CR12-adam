8acfe5857b98c557c2ec5b787b69a7836a7fbdb2
Adam Al-Salehi <bakr.arkan@icloud.com>
2020-11-28 16:52:12 +0100

    first commit

---
+++ index theme/index.php
@@ -0,0 +1,177 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="preconnect" href="https://fonts.gstatic.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com">
+    <link
+        href="https://fonts.googleapis.com/css2?family=Pacifico&family=Playfair+Display:wght@500&family=Yanone+Kaffeesatz&display=swap"
+        rel="stylesheet">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+    <link rel="stylesheet" href="./css/bootstrap.css">
+    <link rel="stylesheet" href="./style.css">
+    <title>Mount Everest</title>
+</head>
+
+<body>
+
+
+    <nav class="navbar navbar-expand-lg navbar-light bg-transparent border-bottom p-4">
+        <button class="navbar-toggler ml-auto" type="button" data-toggle="collapse" data-target="#navbarNav"
+            aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+        </button>
+        <div class="collapse navbar-collapse" id="navbarNav">
+            <ul class="navbar-nav d-flex justify-content-between">
+                <li class="nav-item">
+                    <a class="nav-link text-secondary text-center" href="#">Home</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link text-secondary text-center" href="#">Features</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link text-secondary text-center" href="#">Pricing</a>
+                </li>
+            </ul>
+            <ul class="navbar-nav ml-auto">
+                <li class="nav-item">
+                    <a href="#" class="nav-link text-secondary fa fa-facebook text-center"></a>
+                </li>
+                <li class="nav-item">
+                    <a href="#" class="nav-link text-secondary fa fa-twitter text-center"></a>
+                </li>
+                <li class="nav-item">
+                    <a href="#" class="nav-link text-secondary fa fa-instagram text-center"></a>
+                </li>
+            </ul>
+        </div>
+    </nav>
+
+    <div class="container">
+        <a class="navbar-brand d-flex" href="#"> <img id="logo" class="img-fluid m-auto" src="logo.png" alt="logo"></a>
+    </div>
+    <nav class="navbar navbar-expand navbar-light bg-transparent border-bottom border-top">
+        <div class="navbar m-auto">
+            <ul class="navbar-nav">
+                <li class="nav-item">
+                    <a class="nav-link text-secondary" href="#">Home</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link text-secondary" href="#">Features</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link text-secondary" href="#">Pricing</a>
+                </li>
+            </ul>
+        </div>
+    </nav>
+
+    <div class="container">
+        <div class="row  mt-5">
+            <div class="col-lg-4 col-sm-11 col-md-11  mb-3 ">
+                <div class="card border-0">
+                    <img class="img-fluid card-img m-auto"
+                        src="https://static.travelworks.de/media/header/work-and-travel/seo/work-and-travel-organisation-header-neu-lg.jpg"
+                        alt="placeholder">
+                    <div class="card-body text-center">
+                        <a href="#" class="nav-link text-dark p-0"><h3 class="card-title ">Card Title</h3></a>
+                        <hr class="card-hr">
+                        <p class="card-description">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>
+                        <p class="card-date">12.12.2012</p>
+                    </div>
+                </div>
+            </div>
+            <div class="col-lg-4 col-sm-11 col-md-11  mb-3 ">
+                <div class="card border-0">
+                    <img class="img-fluid card-img m-auto"
+                        src="https://media.gettyimages.com/photos/tourist-visiting-spain-picture-id895081824?s=612x612"
+                        alt="placeholder">
+                    <div class="card-body text-center">
+                        <a href="#" class="nav-link text-dark p-0"><h3 class="card-title">Card Title</h3></a>
+                        <hr class="card-hr">
+                        <p class="card-description">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>
+                        <p class="card-date">12.12.2012</p>
+                    </div>
+                </div>
+            </div>
+            <div class="col-lg-4 col-sm-11 col-md-11  mb-3 ">
+                <div class="card border-0">
+                    <img class="img-fluid card-img m-auto"
+                        src="https://images.pexels.com/photos/3922943/pexels-photo-3922943.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500"
+                        alt="placeholder">
+                    <div class="card-body text-center">
+                        <a href="#" class="nav-link text-dark p-0"><h3 class="card-title">Card Title</h3></a>
+                        <hr class="card-hr">
+                        <p class="card-description">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>
+                        <p class="card-date">12.12.2012</p>
+                    </div>
+                </div>
+            </div>
+            <div class="col-lg-4 col-sm-11 col-md-11  mb-3 ">
+                <div class="card border-0">
+                    <img class="img-fluid card-img m-auto"
+                        src="https://static.travelworks.de/media/header/work-and-travel/seo/work-and-travel-organisation-header-neu-lg.jpg"
+                        alt="placeholder">
+                    <div class="card-body text-center">
+                        <a href="#" class="nav-link text-dark p-0"><h3 class="card-title">Card Title</h3></a>
+                        <hr class="card-hr">
+                        <p class="card-description">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>
+                        <p class="card-date">12.12.2012</p>
+                    </div>
+                </div>
+            </div>
+            <div class="col-lg-4 col-sm-11 col-md-11  mb-3 ">
+                <div class="card border-0">
+                    <img class="img-fluid card-img m-auto"
+                        src="https://static.travelworks.de/media/header/work-and-travel/seo/work-and-travel-organisation-header-neu-lg.jpg"
+                        alt="placeholder">
+                    <div class="card-body text-center">
+                        <a href="#" class="nav-link text-dark p-0"><h3 class="card-title">Card Title</h3></a>
+                        <hr class="card-hr">
+                        <p class="card-description">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>
+                        <p class="card-date">12.12.2012</p>
+                    </div>
+                </div>
+            </div>
+            <div class="col-lg-4 col-sm-11 col-md-11  mb-3 ">
+                <div class="card border-0">
+                    <img class="img-fluid card-img m-auto"
+                        src="https://static.travelworks.de/media/header/work-and-travel/seo/work-and-travel-organisation-header-neu-lg.jpg"
+                        alt="placeholder">
+                    <div class="card-body text-center">
+                        <a href="#" class="nav-link text-dark p-0"><h3 class="card-title">Card Title</h3></a>
+                        <hr class="card-hr">
+                        <p class="card-description">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>
+                        <p class="card-date">12.12.2012</p>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+    <div class="footer bg-secondary py-3">
+        
+        <div class="social-icons d-flex flex-row text-center align-items-center justify-content-center m-auto">
+            <span>Facebook</span><a href="#" class="nav-link text-dark fa fa-facebook"></a>
+            <span>Twitter</span><a href="#" class="nav-link text-dark fa fa-twitter"></a>
+            <span>Instagram</span><a href="#" class="nav-link text-dark fa fa-instagram"></a>
+
+        </div>
+        <hr class="text-dark">
+        <div class="cr d-flex flex-row align-items-center justify-content-center m-auto">
+            <span><i class="fa fa-copyright mr-1"></i></span>
+            <h3 class="text-center">2020-Adam-All rights reserved</h3>
+        </div>
+    </div>
+
+
+    <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js"
+        integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous">
+    </script>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.2.1/js/bootstrap.min.js"
+        integrity="sha384-B0UglyR+jN6CkvvICOB2joaf5I4l3gm9GU6Hc1og6Ls7i6U/mkkaduKaBhlAXv9k" crossorigin="anonymous">
+    </script>
+</body>
+
+</html>
